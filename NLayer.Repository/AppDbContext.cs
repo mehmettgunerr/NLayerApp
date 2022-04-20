d772c26218a5fe39e9abf58359cfd95ea5d440ad
mehmettgunerr <63156838+mehmettgunerr@users.noreply.github.com>
2022-04-20 22:35:16 +0300

    Override savechanges

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -11,7 +11,7 @@ namespace NLayer.Repository
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
@@ -19,6 +19,55 @@ namespace NLayer.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            entityReference.CreatedDate = DateTime.Now;
+                            break;
+                        case EntityState.Modified:
+                            Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                            entityReference.UpdatedDate = DateTime.Now;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            entityReference.CreatedDate = DateTime.Now;
+                            break;
+                        case EntityState.Modified:
+                            Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                            entityReference.UpdatedDate = DateTime.Now;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
